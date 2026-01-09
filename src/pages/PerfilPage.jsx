c1c4ec99760e4d6b9c1dd12ee7b28d7cc42094ea
Diaz Martin Fernando <elmartind17@gmail.com>
2026-01-09 17:24:53 -0300

    Refactor(Perfil-Usuario): Se agrego el sistema para calcular el nivel de la cuenta.

--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -3,9 +3,11 @@ import grid_green from "../icons/grid_green.svg";
 import deseados from "../icons/deseados.svg";
 import JuegosDelUsuario from "../componets/perfil/JuegosDelUsuario";
 import { useObtenerUsuario } from "../services/obtenerUsuario.js";
+import { useCalcularNivel } from "../services/calcularNivel.js";
 
 export default function PerfilPage() {
   const { usuario } = useObtenerUsuario();
+  const { nivel, puntos } = useCalcularNivel();
 
   if (!usuario) {
     return <></>;
@@ -19,16 +21,7 @@ export default function PerfilPage() {
     <section className="perfil_page_section">
       <div className="perfil_page_usuario">
         {foto_de_perfil ? (
-          <img
-            src={foto_de_perfil}
-            alt="Avatar"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              borderRadius: "10px",
-            }}
-          />
+          <img src={foto_de_perfil} alt="Avatar" />
         ) : (
           <p className="iniciales">{iniciales}</p>
         )}
@@ -42,11 +35,11 @@ export default function PerfilPage() {
 
       <div className="perfil_page_nivel">
         <div>
-          <p>22</p>
+          <p>{nivel + 1}</p>
         </div>
         <div>
           <p>Nivel</p>
-          <p>6.400 XP</p>
+          <p>{puntos} XP</p>
         </div>
       </div>
 
