32e9f9c2e14216ba9869acdadd3a7531611fc4f6
Diaz Martin Fernando <elmartind17@gmail.com>
2026-01-13 17:54:17 -0300

    Refactor(categorias-page): Se adapto el diseño para version de escritorio, categorías con diseño glassmorphism y carrusel dinámico.

--- src/constants/categorias.js
+++ src/constants/categorias.js
@@ -13,51 +13,51 @@ export const categorias = [
   {
     nombre: "Acción",
     icon: accion,
-    color: "linear-gradient(to right, #ff416c, #ff6a88)",
+    color: "linear-gradient(to right, #ff416d25, #ff6a88)",
   },
   {
     nombre: "Aventura",
     icon: aventura,
-    color: "linear-gradient(to right, #f7971e, #ffaf4b)",
+    color: "linear-gradient(to right, #f7951e2f, #ffaf4b)",
   },
   {
     nombre: "RPG",
     icon: rpg,
-    color: "linear-gradient(to right, #8360c3, #9d7add)",
+    color: "linear-gradient(to right, #8360c32a, #9d7add)",
   },
   {
     nombre: "Estrategia",
     icon: estrategia,
-    color: "linear-gradient(to right, #0099f7, #4bbbfb)",
+    color: "linear-gradient(to right, #0098f731, #4bbbfb)",
   },
   {
     nombre: "Deportes",
     icon: deportes,
-    color: "linear-gradient(to right, #56ab2f, #7bd154)",
+    color: "linear-gradient(to right, #56ab2f3a, #7bd154)",
   },
   {
     nombre: "Carreras",
     icon: carreras,
-    color: "linear-gradient(to right, #f05053, #ff7678)",
+    color: "linear-gradient(to right, #f050532f, #ff7678)",
   },
   {
     nombre: "Simulación",
     icon: simulacion,
-    color: "linear-gradient(to right, #4568dc, #6a85e6)",
+    color: "linear-gradient(to right, #4568dc28, #6a85e6)",
   },
   {
     nombre: "Terror",
     icon: terror,
-    color: "linear-gradient(to right, #1f1f1f, #3a3a3a)",
+    color: "linear-gradient(to right, #ffffff23, #3a3a3a)",
   },
   {
     nombre: "Indie",
     icon: indie,
-    color: "linear-gradient(to right, #1fa2ff, #5ac8fa)",
+    color: "linear-gradient(to right, #1fa2ff2a, #5ac8fa)",
   },
   {
     nombre: "Sci-Fi",
     icon: scifi,
-    color: "linear-gradient(to right, #cc2b5e, #e64c7b)",
+    color: "linear-gradient(to right, #cc2b5e2f, #e64c7b)",
   },
 ];
--- src/pages/CategoriaSeleccionadaPage.jsx
+++ src/pages/CategoriaSeleccionadaPage.jsx
@@ -1,21 +1,20 @@
 import "../css/categoriaSeleccionada.css";
 import search from "../icons/search.svg";
 import Categoria from "../componets/categorias/Categoria";
+import { useParams } from "react-router-dom";
+import { categorias } from "../constants/categorias";
+import HeroCategoria from "../componets/categorias/HeroCategoria";
 
 export default function CategoriaSeleccionadaPage() {
+  const { id } = useParams();
+
   return (
     <section className="categoria_seleccionada_page">
-      <div className="categoria_seleccionada_search">
-        <div>
-          <img src={search} alt="" />
-          <input
-            onChange={(event) => {
-              setBuscar(event.target.value);
-            }}
-            type="text"
-            placeholder="Buscar categoria..."
-          />
-        </div>
+      <div className="categoria_seleccionada">
+        <p className="categoria_seleccionada_titulo">Categoria / {id}</p>
+        <section className="juego_destacado">
+          <HeroCategoria />
+        </section>
       </div>
 
       <section className="juegos_section">
