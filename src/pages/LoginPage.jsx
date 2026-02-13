ca8d4d4d1b010952710e0684635da8aa6307cb5e
Juan Jose Salvi <salvijuan60@gmail.com>
2026-02-13 18:05:41 -0300

    refactor(google-auth): Se importó el navigate que faltaba en LoginPage y RegisterPage.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Login from "../componets/auth/Login";
 import back from "../icons/back.svg";
 import more from "../icons/more.svg";
+import { useNavigate }  from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   return (
     <>
       <nav className="navbar-phone desk">
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Register from "../componets/auth/Register";
 import back from "../icons/back.svg";
 import more from "../icons/more.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
   return (
     <>
       <nav className="navbar-phone desk">
